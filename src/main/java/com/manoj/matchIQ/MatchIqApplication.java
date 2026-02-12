95ca58872e848992406f8f8c4d6c76e92b27c49e
Manoj Sai Kothapati <manojsaikothapati@gmail.com>
2026-02-12 10:18:15 -0600

    Add files via upload

---
+++ src/main/java/com/manoj/matchIQ/MatchIqApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.matchIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchIqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoj/matchIQ/MatchIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.matchIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
